949ca53bd9c4c26701d4eeef42526d2b3aaa34b7
yukiasai <yukiasai@gmail.com>
2016-02-01 17:27:53 +0900

    Write tests of using Alamofire.

---
+++ KageeTests/AlamofireTests.swift
@@ -0,0 +1,62 @@
+//
+//  AlamofireTests.swift
+//  Kagee
+//
+//  Created by yukiasai on 2/1/16.
+//  Copyright © 2016 yukiasai. All rights reserved.
+//
+
+import XCTest
+import Alamofire
+@testable import Kagee
+
+class AlamofireTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        MockProtocol.register()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+    
+    func testGet() {
+        let ex = expectationWithDescription("")
+        
+        let url = "/"
+        Mock.up().request(url: url).response(200, body: nil, header: nil)
+        Alamofire.request(.GET, url).response { _, response, _, _ in
+            XCTAssertEqual(response?.statusCode, 200)
+            ex.fulfill()
+        }
+        
+        waitForExpectationsWithTimeout(1000) { error in }
+    }
+    
+    func testJSON() {
+        let ex = expectationWithDescription("")
+        
+        let url = "/json"
+        let json: [String: AnyObject] = ["name": "yukiasai", "age": 28]
+        Mock.up().request(url: url).response(200, body: JSON(json), header: nil)
+        Alamofire.request(.GET, url).responseJSON { response in
+            switch response.result {
+            case .Success(let json):
+                guard let dic = json as? [String: AnyObject] else {
+                    XCTFail()
+                    return
+                }
+                XCTAssertEqual(dic["name"] as? String, "yukiasai")
+                XCTAssertEqual(dic["age"] as? Int, 28)
+            case .Failure(_):
+                XCTFail()
+            }
+            
+            ex.fulfill()
+        }
+        
+        waitForExpectationsWithTimeout(1000) { error in }
+    }
+    
+}
